67035862d058972b494a893c7018a7a445233446
Ahmed Yassin <45315700+aahmedYassin@users.noreply.github.com>
2023-07-06 12:01:59 +0300

    Time: 64 ms (36.20%), Space: 43.6 MB (74.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = target - nums[i];
-                if (nums[j] == complement) {
-                    return new int[]{i, j};
-                }
-            }
+   public int[] twoSum(int[] nums, int target) {
+        int indicies[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){  
+                        indicies[0]=i;
+                        indicies[1]=j;
+                        break; 
+                }            
+            }       
         }
-        return new int[]{};
+        return indicies;
     }
 }

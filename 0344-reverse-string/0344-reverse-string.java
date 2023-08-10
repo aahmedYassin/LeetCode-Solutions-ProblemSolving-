c087da9249698a381f7b27a1fa96914bbb1a2a56
Ahmed Yassin <45315700+aahmedYassin@users.noreply.github.com>
2023-08-10 06:51:32 +0300

    Time: 105 ms (5.13%), Space: 49.2 MB (97.77%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        int i = 0, j = s.length - 1;
+        while (i < j) {
+
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+            i++;
+            j--;
+        }
+
+        for (char c : s) {
+            System.out.println(c);
+        }
+
+    }
+}
